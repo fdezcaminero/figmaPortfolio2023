a26c68d7aaf961de969f713293d336f30a5093a4
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-04-10 18:01:05 -0400

    Get toolbar menu working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 function openMenu() {
+  const mobileDisplay = document.getElementById("mobile1");
+  const mobileMenu = document.getElementById("mobile2");
 
+  mobileDisplay.classList.toggle("dispNone");
+  mobileMenu.classList.toggle("dispNone");
 }
 
 function closeMenu() {
